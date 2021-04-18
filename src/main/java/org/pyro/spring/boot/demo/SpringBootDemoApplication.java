7b6eb12c2878f06a0a3c2ad162bc1550a208ae91
MoronSlayer <MoronSlayer@outlook.com>
2021-04-18 10:33:09 +0800

    Init Commit

---
+++ src/main/java/org/pyro/spring/boot/demo/SpringBootDemoApplication.java
@@ -0,0 +1,18 @@
+package org.pyro.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author ZYC
+ * @date 2021/4/18 10:21
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+    
+}
