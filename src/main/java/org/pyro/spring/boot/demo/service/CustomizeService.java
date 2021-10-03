eeac0d7e3aa51a61f93b3ce22498684b7ea4cd91
MoronSlayer <MoronSlayer@outlook.com>
2021-10-03 21:54:38 +0800

    fix: 环境变量

--- src/main/java/org/pyro/spring/boot/demo/service/CustomizeService.java
+++ src/main/java/org/pyro/spring/boot/demo/service/CustomizeService.java
@@ -3,6 +3,7 @@ package org.pyro.spring.boot.demo.service;
 import lombok.extern.slf4j.Slf4j;
 import org.pyro.spring.boot.demo.config.CustomizeProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -21,10 +22,14 @@ public class CustomizeService {
     private ApplicationContext applicationContext;
     
     @Resource
+    private Environment environment;
+    
+    @Resource
     private CustomizeProperties config;
     
     public void test() {
         log.info(Objects.toString(this.applicationContext));
+        log.info(Objects.toString(this.environment));
         log.info(Objects.toString(config));
     }
     
